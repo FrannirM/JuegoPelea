10c2e79841f2115f28533acf2b5cbe52e254335d
Franco Martínez <francomartinez353@yahoo.com.ar>
2025-07-04 19:36:04 -0300

    Sigo ordenando la logica, termine un metodo para elegir el primer modificador de daño pero tengo que hacer el otro

--- src/juego/Habilidades.java
+++ src/juego/Habilidades.java
@@ -9,26 +9,20 @@ public class Habilidades {
     final private double jab = 16.7;
     final private double derechaDos = 38.5;
     final private double lowkick = 22.4;
-    final private double croche = 44;
-    final private double highkick = 77.5;
-    final private double codazo = 19.7;
-    final private double rodillazo = 55.3;
 
     //grappling
     final private double mataleon = 14;
     final private double doubleleg = 32.5;
-    final private double leg = 12.1;
-    final private double osotogari = 67.33;
-    final private double leglock = 11;
-    final private double grandandpound = 79.8;
-    final private double triangulo = 57.3;
+    final private double osotogari = 31.33;
 
-    //Metodo para elegir primer modificador
-    static int elegirBackground(int modificadorBackground) {
-
+    //Metodo para elegir primer modificador ||
+    static void elegirBackground() {
+        int tipoBackground1 = 0, tipoBackground2 = 0;
         var salida = false;
+        var consola = new Scanner(System.in);
 
         while (!salida) {
+
             System.out.println("Jugador 1 Elegi tu Background: ");
             System.out.println("""
                     1-Boxeo
@@ -37,10 +31,78 @@ public class Habilidades {
                     4-BJJ
                     5-Muay Thai
                     """);
-            var tipoBackground = Integer.parseInt(var consola = new Scanner(System.in));
+             tipoBackground1 = Integer.parseInt(consola.nextLine());
+
+            if (tipoBackground1 == 1 || tipoBackground1 == 2 || tipoBackground1 == 3 || tipoBackground1 == 4 || tipoBackground1 == 5) {
+                System.out.println("Jugador 2 Elegi tu Background: ");
+                System.out.println("""
+                    1-Boxeo
+                    2-Karate
+                    3-Lucha
+                    4-BJJ
+                    5-Muay Thai
+                    """);
+                tipoBackground2 = Integer.parseInt(consola.nextLine());
+                salida = true;
+            } else System.out.println("ERROR, VUELVA A INTENTARLO");
         }
 
-        return modificadorBackground;
+    }
+
+
+
+    //Metodo para elegir segundo modificador ||
+    static void elegirHabilidades() {
+        int tipoHabilidad1 = 0, tipoHabilidad2 = 0;
+        var salida = false;
+        var consola = new Scanner(System.in);
+
+        while (!salida) {
+
+            System.out.println("Jugador 1 Elegi tus habilidades, solo pueden ser tres: ");
+            System.out.println("""
+                    1-Jab
+                    2-Mataleon
+                    3-Derechazo
+                    4-Double Leg
+                    5-LowKick
+                    6-OsotoGari
+                    """);
+            for (var i = 0; i < 3; i++) {
+                tipoHabilidad1 = Integer.parseInt(consola.nextLine());
+                switch (tipoHabilidad1) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+            if (tipoHabilidad1 == 1) {
+                System.out.println("Jugador 2 Elegi tu Background: ");
+                System.out.println("""
+                    1-Boxeo
+                    2-Karate
+                    3-Lucha
+                    4-BJJ
+                    5-Muay Thai
+                    """);
+                tipoBackground2 = Integer.parseInt(consola.nextLine());
+                salida = true;
+            } else System.out.println("ERROR, VUELVA A INTENTARLO");
+        }
+
     }
 
 }
